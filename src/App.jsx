29cf62b30115f9831d411fcdb8ec0251300ff27f
Daniel González Briseño <daniel.gb@globant.com>
2022-06-25 07:20:45 -0500

    02 - Folder Structure

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-const App = () => {
-  return (
-    <h1>Journal App</h1>
-  );
-};
-
-export default App;
---
+++ src/JournalApp.jsx
@@ -0,0 +1,7 @@
+const JournalApp = () => {
+  return (
+    <h1>Journal App</h1>
+  );
+};
+
+export default JournalApp;
