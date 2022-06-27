813666372cd6fcbd7ce2724477b7ffe17ce16917
Daniel González Briseño <daniel.gb@globant.com>
2022-06-27 16:57:38 -0500

    28 - Checking Authentication

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,18 @@
+import { useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 
 import { AuthRoutes } from "../auth";
 import { JournalRoutes } from "../journal";
+import { CheckingAuth } from "../ui";
 
 export const AppRouter = () => {
+
+  const { status } = useSelector(({ auth }) => auth );
+
+  if ( status === 'checking' ) {
+    return <CheckingAuth />;
+  }
+
   return (
     <Routes>
       <Route path="/auth/*" element={ <AuthRoutes /> } />
